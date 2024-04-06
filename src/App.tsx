d8ff37623d86ef09919af58b02a059e6d009bbf7
SeungminCha <dominico1225@gmail.com>
2024-04-06 17:14:58 +0900

    ftr: reusable button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import '@/index.css'
-import { css } from '@styled-stytem/css'
+import Button from '@/components/Button.tsx'
 
 function App() {
-  return <div className={css({ fontSize: '4xl', fontWeight: 'bold', color: 'green.100' })}>Hello 🐼!</div>
+  const handleClick = (varint: string) => {
+    alert(varint)
+  }
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: '#242424',
+        gap: 10
+      }}
+    >
+      <Button variant="primary" onClick={() => handleClick('primary')}>
+        Primary
+      </Button>
+      <Button variant="secondary" onClick={() => handleClick('secondary')}>
+        Secondary
+      </Button>
+    </div>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from '@/App.tsx'
+import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
