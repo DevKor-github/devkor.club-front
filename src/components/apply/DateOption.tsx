0677dcf2fdbc924cd54747dc88444112d85ef9cb
overthestream <njhnjh02@naver.com>
2024-07-18 12:30:59 +0900

    fix

--- src/components/apply/DateOption.tsx
+++ src/components/apply/DateOption.tsx
@@ -1,53 +1,52 @@
-import { css } from '@styled-stytem/css'
-import { AnimatePresence, motion } from 'framer-motion'
-import { memo, useState } from 'react'
+import { css } from "@styled-stytem/css";
+import { AnimatePresence, motion } from "framer-motion";
+import { memo, useState } from "react";
 
-import Checked from '@/assets/Checked.svg'
-import CheckedHover from '@/assets/CheckedHover.svg'
-import Unchecked from '@/assets/Unchecked.svg'
-import UncheckedHover from '@/assets/UncheckedHover.svg'
+import Checked from "@/assets/Checked.svg";
+import Unchecked from "@/assets/Unchecked.svg";
 
 interface DateOptionProps {
-  index: number
-  dateOption: string
-  selected: boolean
-  handleSelecteTime: (index: number) => void
+	index: number;
+	dateOption: string;
+	selected: boolean;
+	handleSelecteTime: (index: number) => void;
 }
-const DateOption = memo(({ index, dateOption, selected, handleSelecteTime }: DateOptionProps) => {
-  const [hover, setHover] = useState(false)
-  //   const [selectedTime, setSelectedTime] = useAtom(selectedTimes)
-  //   console.log(selectedTime)
-  return (
-    <button
-      className={css({
-        display: 'flex',
-        w: 'full',
-        pos: 'relative',
-        maxW: 820,
-        h: 53,
-        px: 30,
-        py: '13px',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        alignSelf: 'stretch',
-        rounded: 30,
-        border: '1.5px solid {colors.label.98}',
-        bgColor: 'label.BG',
-        _hover: { bgColor: 'label.98' },
-        transition: 'background-color 0.3s ease-in-out',
-        cursor: 'pointer',
-        gap: 2.5
-      })}
-      //   onMouseEnter={() => setHover(h => !h)}
-      //   onMouseLeave={() => setHover(h => !h)}
-      onClick={e => {
-        e.preventDefault()
-        handleSelecteTime(index)
-        // setSelectedTime(t => t.map((time, i) => (i === index ? { selected: !time.selected } : time)))
-      }}
-    >
-      <AnimatePresence>
-        {/* {hover && (
+const DateOption = memo(
+	({ index, dateOption, selected, handleSelecteTime }: DateOptionProps) => {
+		const [hover] = useState(false);
+		//   const [selectedTime, setSelectedTime] = useAtom(selectedTimes)
+		//   console.log(selectedTime)
+		return (
+			<button
+				className={css({
+					display: "flex",
+					w: "full",
+					pos: "relative",
+					maxW: 820,
+					h: 53,
+					px: 30,
+					py: "13px",
+					justifyContent: "flex-start",
+					alignItems: "center",
+					alignSelf: "stretch",
+					rounded: 30,
+					border: "1.5px solid {colors.label.98}",
+					bgColor: "label.BG",
+					_hover: { bgColor: "label.98" },
+					transition: "background-color 0.3s ease-in-out",
+					cursor: "pointer",
+					gap: 2.5,
+				})}
+				//   onMouseEnter={() => setHover(h => !h)}
+				//   onMouseLeave={() => setHover(h => !h)}
+				onClick={(e) => {
+					e.preventDefault();
+					handleSelecteTime(index);
+					// setSelectedTime(t => t.map((time, i) => (i === index ? { selected: !time.selected } : time)))
+				}}
+			>
+				<AnimatePresence>
+					{/* {hover && (
           <motion.img
             key={`uncheckedHover-${index}`}
             src={UncheckedHover}
@@ -58,18 +57,18 @@ const DateOption = memo(({ index, dateOption, selected, handleSelecteTime }: Dat
             exit={{ opacity: 0 }}
           />
         )} */}
-        {!hover && (
-          <motion.img
-            key={`unchecked-${index}`}
-            src={Unchecked}
-            alt="unchecked"
-            className={css({ w: '26px', pos: 'absolute' })}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          />
-        )}
-        {/* {hover && selectedTime[index].selected && (
+					{!hover && (
+						<motion.img
+							key={`unchecked-${index}`}
+							src={Unchecked}
+							alt="unchecked"
+							className={css({ w: "26px", pos: "absolute" })}
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+						/>
+					)}
+					{/* {hover && selectedTime[index].selected && (
           <motion.img
             key={`checkedHover-${index}`}
             src={CheckedHover}
@@ -80,21 +79,31 @@ const DateOption = memo(({ index, dateOption, selected, handleSelecteTime }: Dat
             exit={{ opacity: 0 }}
           />
         )} */}
-        {!hover && selected && (
-          <motion.img
-            key={`checked-${index}`}
-            src={Checked}
-            alt="unchecked"
-            className={css({ w: '26px', pos: 'absolute' })}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          />
-        )}
-      </AnimatePresence>
-      <p className={css({ fontSize: 18, fontWeight: 500, color: 'label.80', ml: 10 })}>{dateOption}</p>
-    </button>
-  )
-})
+					{!hover && selected && (
+						<motion.img
+							key={`checked-${index}`}
+							src={Checked}
+							alt="unchecked"
+							className={css({ w: "26px", pos: "absolute" })}
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+						/>
+					)}
+				</AnimatePresence>
+				<p
+					className={css({
+						fontSize: 18,
+						fontWeight: 500,
+						color: "label.80",
+						ml: 10,
+					})}
+				>
+					{dateOption}
+				</p>
+			</button>
+		);
+	},
+);
 
-export default DateOption
+export default DateOption;
--- src/components/apply/Portfolio.tsx
+++ src/components/apply/Portfolio.tsx
@@ -1,97 +1,125 @@
-import { css } from '@styled-stytem/css'
-import { motion } from 'framer-motion'
-import { CircleX, FileText, Upload } from 'lucide-react'
-import { useCallback, useState } from 'react'
-import { createPortal } from 'react-dom'
-import { useParams } from 'react-router-dom'
+import { css } from "@styled-stytem/css";
+import { motion } from "framer-motion";
+import { CircleX, FileText, Upload } from "lucide-react";
+import { useCallback, useState } from "react";
+import { createPortal } from "react-dom";
+import { useParams } from "react-router-dom";
 
-import FileUploadModal from '@/components/apply/FileUploadModal'
-import Button from '@/components/ui/button'
-import { useMatchLayout } from '@/utils/useMatchLayout'
+import FileUploadModal from "@/components/apply/FileUploadModal";
+import Button from "@/components/ui/button";
+import { useMatchLayout } from "@/utils/useMatchLayout";
 
 interface PortfolioProps {
-  fileName: string | null
-  originalFileName: string | null
-  handleFileSelect: (fileName: string, originalFileName: string, url) => void
-  handleDeleteFile: () => void
+	fileName: string | null;
+	originalFileName: string | null;
+	handleFileSelect: (
+		fileName: string,
+		originalFileName: string,
+		url: string,
+	) => void;
+	handleDeleteFile: () => void;
 }
-const Portfolio = ({ fileName, originalFileName, handleFileSelect, handleDeleteFile }: PortfolioProps) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [deleteFile, setDeleteFile] = useState(false)
-  const handleCloseModal = useCallback(() => setIsOpen(false), [])
+const Portfolio = ({
+	fileName,
+	originalFileName,
+	handleFileSelect,
+	handleDeleteFile,
+}: PortfolioProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [deleteFile, setDeleteFile] = useState(false);
+	const handleCloseModal = useCallback(() => setIsOpen(false), []);
 
-  const mediaQuery = useMatchLayout()
-  const { track } = useParams()
-  return (
-    <>
-      <div
-        className={css({
-          display: 'flex',
-          px: 5,
-          gap: 1,
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-          alignSelf: 'stretch',
-          whiteSpace: 'pre-wrap',
-          color: 'label.80',
-          S: { fontSize: 16, fontWeight: 600 },
-          XS: { fontSize: 14, fontWeight: 600 }
-        })}
-      >
-        {track === 'DE' ? '포트폴리오를 첨부해주세요!' : ' 포트폴리오가 있다면 첨부해주세요! '}
-        <p className={css({ fontSize: 12, fontWeight: 400 })}>500MB 이하의 파일만 업로드 가능합니다.</p>
-        {track === 'DE' && (
-          <div
-            className={css({
-              w: 1.5,
-              h: 1.5,
-              rounded: 'full',
-              bgColor: 'secondary.70'
-            })}
-          />
-        )}
-      </div>
-      <div className={css({ display: 'flex', gap: 2.5 })}>
-        <Button variant="icon" type="button" size="icon" onClick={() => setIsOpen(true)}>
-          {fileName ? '파일 변경' : '파일 추가'}
-          <Upload className={css({ w: 5, h: 5 })} />
-        </Button>
-        {originalFileName && (
-          <div
-            className={css({
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              gap: 1
-            })}
-          >
-            <Button variant="iconActive" size="icon" type="button" onClick={() => setDeleteFile(f => !f)}>
-              <FileText className={css({ w: 5, h: 5 })} />
-              {mediaQuery.XSDown && originalFileName.length > 4
-                ? `${originalFileName.slice(0, 4)}...${originalFileName.split('.').pop()}`
-                : originalFileName}
-            </Button>
-            {deleteFile && (
-              <motion.button
-                initial={{ x: -50, opacity: 0 }}
-                animate={deleteFile ? { x: 0, opacity: 1 } : undefined}
-                type="button"
-                className={css({ cursor: 'pointer' })}
-                onClick={handleDeleteFile}
-                disabled={!deleteFile}
-              >
-                <CircleX className={css({ color: 'label.80' })} />
-              </motion.button>
-            )}
-          </div>
-        )}
-      </div>
-      {createPortal(
-        isOpen && <FileUploadModal onChangeFile={handleFileSelect} handleClose={handleCloseModal} />,
-        document.body
-      )}
-    </>
-  )
-}
+	const mediaQuery = useMatchLayout();
+	const { track } = useParams();
+	return (
+		<>
+			<div
+				className={css({
+					display: "flex",
+					px: 5,
+					gap: 1,
+					justifyContent: "flex-start",
+					alignItems: "center",
+					alignSelf: "stretch",
+					whiteSpace: "pre-wrap",
+					color: "label.80",
+					S: { fontSize: 16, fontWeight: 600 },
+					XS: { fontSize: 14, fontWeight: 600 },
+				})}
+			>
+				{track === "DE"
+					? "포트폴리오를 첨부해주세요!"
+					: " 포트폴리오가 있다면 첨부해주세요! "}
+				<p className={css({ fontSize: 12, fontWeight: 400 })}>
+					500MB 이하의 파일만 업로드 가능합니다.
+				</p>
+				{track === "DE" && (
+					<div
+						className={css({
+							w: 1.5,
+							h: 1.5,
+							rounded: "full",
+							bgColor: "secondary.70",
+						})}
+					/>
+				)}
+			</div>
+			<div className={css({ display: "flex", gap: 2.5 })}>
+				<Button
+					variant="icon"
+					type="button"
+					size="icon"
+					onClick={() => setIsOpen(true)}
+				>
+					{fileName ? "파일 변경" : "파일 추가"}
+					<Upload className={css({ w: 5, h: 5 })} />
+				</Button>
+				{originalFileName && (
+					<div
+						className={css({
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							gap: 1,
+						})}
+					>
+						<Button
+							variant="iconActive"
+							size="icon"
+							type="button"
+							onClick={() => setDeleteFile((f) => !f)}
+						>
+							<FileText className={css({ w: 5, h: 5 })} />
+							{mediaQuery.XSDown && originalFileName.length > 4
+								? `${originalFileName.slice(0, 4)}...${originalFileName.split(".").pop()}`
+								: originalFileName}
+						</Button>
+						{deleteFile && (
+							<motion.button
+								initial={{ x: -50, opacity: 0 }}
+								animate={deleteFile ? { x: 0, opacity: 1 } : undefined}
+								type="button"
+								className={css({ cursor: "pointer" })}
+								onClick={handleDeleteFile}
+								disabled={!deleteFile}
+							>
+								<CircleX className={css({ color: "label.80" })} />
+							</motion.button>
+						)}
+					</div>
+				)}
+			</div>
+			{createPortal(
+				isOpen && (
+					<FileUploadModal
+						onChangeFile={handleFileSelect}
+						handleClose={handleCloseModal}
+					/>
+				),
+				document.body,
+			)}
+		</>
+	);
+};
 
-export default Portfolio
+export default Portfolio;
