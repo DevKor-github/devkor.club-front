6933cce7019c6bc9cbf3a7bb19bf0a7d9645a618
SeungminCha <dominico1225@gmail.com>
2025-02-09 15:49:45 +0900

    feat: amplitude 단일 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { init } from '@amplitude/analytics-browser'
+import { useEffect } from 'react'
 import { useRoutes } from 'react-router-dom'
 
 import routes from '@/router'
 
 function App() {
-  init(import.meta.env.VITE_API_AMPLITUDE_API_KEY)
-    .promise.then(() => {
-      console.log('[[[[[Amplitude Initiated]]]]]')
-    })
-    .catch(error => {
-      console.log('[[[[[Amplitude Init Error]]]]]', error)
-    })
+  useEffect(() => {
+    init(import.meta.env.VITE_API_AMPLITUDE_API_KEY)
+      .promise.then(() => {
+        console.log('[[[[[Amplitude Initiated]]]]]')
+      })
+      .catch(error => {
+        console.log('[[[[[Amplitude Init Error]]]]]', error)
+      })
+  }, [])
   const route = useRoutes(routes)
   return route
 }
