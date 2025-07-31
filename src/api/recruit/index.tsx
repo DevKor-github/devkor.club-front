1b62e0d7fbdcb285af70cbcf34c927452cd95427
SeungminCha <dominico1225@gmail.com>
2025-07-31 23:53:26 +0900

    fix: presigned-url 응답 값 반영 수정

--- src/api/recruit/index.tsx
+++ src/api/recruit/index.tsx
@@ -1,10 +1,11 @@
 import { apiClient } from '@/api/apiClient'
+import { CommonResponse } from '@/domain/common/types'
 
 export const fetchPresignedUrl = async (fileName: string) => {
-  const response = await apiClient.post('/presigned-url', {
+  const response = await apiClient.post<CommonResponse<string>>('/presigned-url', {
     fileName
   })
-  return response.data
+  return response.data.data
 }
 
 export const uploadImageToS3 = async (presignedUrl: string, file: File) => {
